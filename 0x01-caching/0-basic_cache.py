f3ce45bc83ecdf91c562b91607c76e05572cb33f
Elpira <elpira@elpira.168.1.36>
2024-10-30 17:54:03 +0100

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,7 +11,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """put"""
-        if key and item:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
