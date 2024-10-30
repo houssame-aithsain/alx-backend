9c08e0d38231c86d68f03142f482b50178be6bfc
Elpira <elpira@elpira.168.1.36>
2024-10-30 17:40:40 +0100

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """put"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get"""
+        if key and key in self.cache_data:
+            return self.cache_data[key]
