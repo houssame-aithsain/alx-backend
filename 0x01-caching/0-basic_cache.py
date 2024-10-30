f902b97172ab736848658be63b506ec7955550e1
Elpira <elpira@elpira.168.1.36>
2024-10-30 19:57:13 +0100

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-"""
-BasicCache module
-"""
+""" BasicCache module for caching system """
 
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """basicCache"""
+    """ BasicCache class inherits from BaseCaching with no item limit """
 
     def put(self, key, item):
-        """put"""
+        """ Adds item to cache_data dictionary with key as identifier """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get"""
-        return self.cache_data.get(key, None)
+        """ Returns the item linked to key in cache_data """
+        return self.cache_data.get(key) if key is not None else None
