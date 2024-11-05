5a480e9447d380b37886d8accfec82f035a19589
Elpira <elpira@elpira.168.1.36>
2024-11-05 21:30:13 +0100

    3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
 """
-A Basic flask application
+Basic Flask app with a single route and template
 """
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask_babel import Babel
+from flask import Flask, request, render_template
+from flask_babel import Babel, _
 
 
 class Config(object):
     """
-    Application configuration class
+    Config class for the application
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Wrap the application with Babel
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """
-    Gets locale from request object
+    Determines the best match for the supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -36,9 +32,10 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Renders a basic html template
+    Route to render the template
     """
-    return render_template('3-index.html')
+    return render_template('3-index.html', home_title=_('home_title'),
+                           home_header=_('home_header'))
 
 
 if __name__ == '__main__':
