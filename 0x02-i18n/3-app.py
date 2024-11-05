8e3baed12c9ae55859916301c8d6283c2f5c45d3
Elpira <elpira@elpira.168.1.36>
2024-11-05 21:33:36 +0100

    3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 """
-Basic Flask app with a single route and template
+A simple Flask application with internationalization (i18n) support.
 """
 from flask import Flask, request, render_template
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
-class Config(object):
+class Config:
     """
-    Config class for the application
+    Configuration class for the Flask application.
+
+    This class defines the supported languages and sets the default locale
+    and timezone for the application.
     """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ['en', 'fr']  # Supported languages
+    BABEL_DEFAULT_LOCALE = 'en'  # Default language
+    BABEL_DEFAULT_TIMEZONE = 'UTC'  # Default timezone
 
 
+# Create an instance of the Flask application
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config)  # Load configuration from the Config class
 
+# Initialize Babel for language translations
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """
-    Determines the best match for the supported languages
+    Determines the best match for the user's.
+
+    This function checks the request's accept_languages and returns the
+    best match based on the configured supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -32,11 +40,12 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Route to render the template
+    Renders the index page with an HTML template.
+
+    The index page displays a simple welcome message.
     """
-    return render_template('3-index.html', home_title=_('home_title'),
-                           home_header=_('home_header'))
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run()  # Run the Flask application
